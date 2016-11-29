df727fa95a5b465d74746cd0eaa6c5ca645efa9b
Paul Sarando <psarando@cyverse.org>
2016-11-29 15:23:00 -0700

    Version 2.8.2-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.cyverse/metadata-client "2.8.1"
+(defproject org.cyverse/metadata-client "2.8.2-SNAPSHOT"
   :description "Client for the metadata service"
   :url "https://github.com/cyverse-de/metadata-client"
   :license {:name "BSD"
