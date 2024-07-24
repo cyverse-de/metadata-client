4d39caafa571c896fc16159ca2975135e76fadd2
Sarah Roberts <sonoran.sarah@gmail.com>
2024-07-23 18:06:12 -0700

    CORE-2006: updated dependencies and .gitignore

--- project.clj
+++ project.clj
@@ -2,13 +2,15 @@
   :description "Client for the metadata service"
   :url "https://github.com/cyverse-de/metadata-client"
   :license {:name "BSD"
-            :url "http://cyverse.org/sites/default/files/iPLANT-LICENSE.txt"}
+            :url "https://cyverse.org/license"}
   :deploy-repositories [["releases" :clojars]
                         ["snapshots" :clojars]]
-  :plugins [[test2junit "1.2.2"]]
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [clj-http "2.0.0"]
+  :plugins [[jonase/eastwood "1.4.3"]
+            [lein-ancient "0.7.0"]
+            [test2junit "1.4.4"]]
+  :dependencies [[org.clojure/clojure "1.11.3"]
+                 [clj-http "3.13.0"]
                  [com.cemerick/url "0.1.1" :exclusions [com.cemerick/clojurescript.test]]
-                 [cheshire "5.5.0"]
-                 [medley "1.0.0"]
-                 [org.cyverse/kameleon "3.0.0"]])
+                 [cheshire "5.13.0"]
+                 [medley "1.4.0"]
+                 [org.cyverse/kameleon "3.0.10"]])
